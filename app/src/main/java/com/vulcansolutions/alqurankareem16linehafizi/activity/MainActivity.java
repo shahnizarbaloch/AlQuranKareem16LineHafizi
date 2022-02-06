2de0a491ef0a9e84d4bba9d6c24c8c18b31a8de5
Shah Nizar Baloch <shahnizarbaloch@gmail.com>
2022-02-06 17:24:14 +0500

    - Font added.

--- app/src/main/java/com/vulcansolutions/alqurankareem16linehafizi/activity/MainActivity.java
+++ app/src/main/java/com/vulcansolutions/alqurankareem16linehafizi/activity/MainActivity.java
@@ -1,14 +1,39 @@
 package com.vulcansolutions.alqurankareem16linehafizi.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.vulcansolutions.alqurankareem16linehafizi.R;
 import com.vulcansolutions.alqurankareem16linehafizi.databinding.ActivityMainBinding;
 
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * This method is for font
+     * @param newBase it is Context
+     */
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/font.ttf")
+                                .setFontAttrId(io.github.inflationx.calligraphy3.R.attr.fontPath)
+                                .build()))
+                .build());
+
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
